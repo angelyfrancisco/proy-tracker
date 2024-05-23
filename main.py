ae71cffc2314744cf1770a9ef4cba0bdf661a9b2
Angel Francisco <franciscoangel807@gmail.com>
2024-05-23 18:50:24 -0500

    Once again

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 def main():
+    # Greet user with a message
     
+    # Prompt user to know if they are new member or already have a profile 
+
+    # If not, create a profile
+    # Prompt for basic information regarding person
+    # Ask for amounts he wants to save
+    
+    # If already has a profile
+    #   Display if he wants to make a change
+    #   If not display the amount he needs to save each month 
 
 
 def user_info():
-    input
+
     
     
